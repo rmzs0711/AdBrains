b0898c12268feaea10d5e2b2debc88c94116c7fe
Ramazan Dzhekshembaev <ramazan.dzhekshembaev@jetbrains.com>
2025-05-08 16:04:06 +0200

    Update base URL in Vite config
    The base URL in `vite.config.ts` was updated to '/AdBrains/'. This ensures the application references the correct path for assets and routing.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-vite-app/' // Replace 'my-vite-app' with your repository name
+  base: '/AdBrains/' // Updated to match your repository name
 })
